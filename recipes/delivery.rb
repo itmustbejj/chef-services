f5d6b2fc91de7d84e00f3a2d5c33ac924fae3e5b
Josh Hudson <jhudson@chef.io>
2017-09-01 13:38:18 -0600

    Change location of delivery.license
    Signed-off-by: Josh Hudson <jhudson@chef.io>

--- recipes/delivery.rb
+++ recipes/delivery.rb
@@ -20,7 +20,7 @@ chef_automate node['chef_automate']['fqdn'] do
   builder_pem delivery_databag['builder_pem']
   config delivery_config
   enterprise 'test'
-  license ::File.read('/home/ec2-user/chef_automate.license')
+  license ::File.read('/tmp/chef_automate.license')
   accept_license node['chef-services']['accept_license']
   notifies :run, 'ruby_block[add automate password to databag]', :immediately
 end
